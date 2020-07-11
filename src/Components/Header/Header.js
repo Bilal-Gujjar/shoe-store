597b99683389f86f75d03f42f2b2c900cfd8444b
Bilal-Gujjar <Bilaltahir515@gmail.com>
2020-07-11 15:39:40 +0500

    Button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 function Header() {
     return (
-        <nav>
+        <nav><h1>
             <Link to = 'home'>Home</Link>{' |  '}
-           <Link to = 'product'>Products</Link> 
+            <Link to = 'product'>Products And Other</Link> {'|   '}
+            <Link to = 'contact'>Contact</Link>
+           </h1>
         </nav>
     )
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import { Link,} from 'react-router-dom'
+
+
 
 function Home() {
     return (
-        <div>
-            Welcome to Home Page
+        <div  className ="App-header">
+           <h1>Welcome to Home Page</h1> <div>
+           <h1>Chose the Brand</h1></div>
+           <button >  <Link path to = '../Product'>Nike</Link></button>
+           <hr/>
+           <button>  <Link path to = '../Product'>GUCCi</Link></button>
+           <hr/>
+           <button>  <Link path to = '../Product'>Adidas</Link></button>
+           <hr/>
+           <button>  <Link path to = '../Product'>Jordan</Link></button>
+        
+            
+           
+           
+         
+          
+                   
         </div>
+
     )
 }
 
+
 export default Home
+/*<h3 >Nike</h3>
+<h3>Bata</h3>
+<h3>GUCCI</h3>
+<h3>Importated</h3>
+*/
